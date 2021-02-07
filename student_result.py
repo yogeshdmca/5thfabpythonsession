bc8b1064ba53d622d6fb40621cfdadf44e7b2301
Yogesh Dwivedi <django.work@gmail.com>
2021-02-07 16:40:06 +0530

    added student marksheet test

---
+++ student_result.py
@@ -0,0 +1,10 @@
+#it will work like 2nd.py using while loop and with given features
+
+"""
+1) It will ask for 1)add new student, 2) check student result, 3) add marks for student, 4) delete student,5 exit
+2) Add student will add student data (Name, Role number, f_name, school, class)
+3) add student marks will add marks for student , need to provide role number for adding marks,(math, 100, 78,) after adding all subject , fail and pass will calculated by system.
+4) show student result will show result with all subject marks in proper formate of marksheet
+
+
+"""
